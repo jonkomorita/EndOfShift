a914adcab112d0db4e5b467ed636f07d2a123241
Jonathyn Komorita <iamjonkomo@gmail.com>
2021-01-08 22:24:40 -0800

    All ET's have same TextChanged implementation

--- EndOfShift/MainActivity.cs
+++ EndOfShift/MainActivity.cs
@@ -12,7 +12,8 @@ namespace EndOfShift
     public class MainActivity : AppCompatActivity
     {
         
-        ArrayList total_values = new ArrayList();
+        ArrayList editTextList = new ArrayList();
+        ArrayList textViewList = new ArrayList();
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -73,113 +74,152 @@ namespace EndOfShift
                 txtTotal.Text = Support.calculateTotalRegister(vals).ToString();
             };
 
-            etFifties.TextChanged += (object sender, Android.Text.TextChangedEventArgs e) =>
-            {
-                ArrayList vals = Support.getValues(Support.parseValues(etHundreds, etFifties, etTwenties, etTens, etFives, etOnes, etTwos,
-                                                        etQuarters, etDimes, etNickels, etPennies, etDollars, etHalfDollars));
-                txtFifties.Text = vals[1].ToString();
-                txtTotal.Text = Support.calculateTotalRegister(vals).ToString();
-            };
-
-            etTwenties.TextChanged += (object sender, Android.Text.TextChangedEventArgs e) =>
-            {
-                ArrayList vals = Support.getValues(Support.parseValues(etHundreds, etFifties, etTwenties, etTens, etFives, etOnes, etTwos,
-                                                        etQuarters, etDimes, etNickels, etPennies, etDollars, etHalfDollars));
-                txtTwenties.Text = vals[2].ToString();
-                txtTotal.Text = Support.calculateTotalRegister(vals).ToString();
-            };
-
-            etTens.TextChanged += (object sender, Android.Text.TextChangedEventArgs e) =>
-            {
-                ArrayList vals = Support.getValues(Support.parseValues(etHundreds, etFifties, etTwenties, etTens, etFives, etOnes, etTwos,
-                                                        etQuarters, etDimes, etNickels, etPennies, etDollars, etHalfDollars));
-                txtTens.Text = vals[3].ToString();
-                txtTotal.Text = Support.calculateTotalRegister(vals).ToString();
-            };
-
-            etFives.TextChanged += (object sender, Android.Text.TextChangedEventArgs e) =>
-            {
-                ArrayList vals = Support.getValues(Support.parseValues(etHundreds, etFifties, etTwenties, etTens, etFives, etOnes, etTwos,
-                                                        etQuarters, etDimes, etNickels, etPennies, etDollars, etHalfDollars));
-                txtFives.Text = vals[4].ToString();
-                txtTotal.Text = Support.calculateTotalRegister(vals).ToString();
-            };
-
-            etOnes.TextChanged += (object sender, Android.Text.TextChangedEventArgs e) =>
-            {
-                ArrayList vals = Support.getValues(Support.parseValues(etHundreds, etFifties, etTwenties, etTens, etFives, etOnes, etTwos,
-                                                        etQuarters, etDimes, etNickels, etPennies, etDollars, etHalfDollars));
-                txtOnes.Text = vals[5].ToString();
-                txtTotal.Text = Support.calculateTotalRegister(vals).ToString();
-            };
-
-            etTwos.TextChanged += (object sender, Android.Text.TextChangedEventArgs e) =>
-            {
-                ArrayList vals = Support.getValues(Support.parseValues(etHundreds, etFifties, etTwenties, etTens, etFives, etOnes, etTwos,
-                                                        etQuarters, etDimes, etNickels, etPennies, etDollars, etHalfDollars));
-                txtTwos.Text = vals[6].ToString();
-                txtTotal.Text = Support.calculateTotalRegister(vals).ToString();
-            };
+            etFifties.TextChanged += ETextChanged;
+            etTwenties.TextChanged += ETextChanged;
+            etTens.TextChanged += ETextChanged;
+            etFives.TextChanged += ETextChanged;
+            etOnes.TextChanged += ETextChanged;
+            etTwos.TextChanged += ETextChanged;
 
             //TextChanged Events [Coins]
-            etQuarters.TextChanged += (object sender, Android.Text.TextChangedEventArgs e) =>
+            etQuarters.TextChanged += ETextChanged;
+            etDimes.TextChanged += ETextChanged;
+            etNickels.TextChanged += ETextChanged;
+            etPennies.TextChanged += ETextChanged;
+            etDollars.TextChanged += ETextChanged;
+            etHalfDollars.TextChanged += ETextChanged;
+        }
+        /**
+          * 0 - hundreds
+          * 1 - fifties
+          * 2 - twenties
+          * 3 - tens
+          * 4 - fives
+          * 5 - ones
+          * 6 - twos
+          * 7 - quarters
+          * 8 - dimes
+          * 9 - nickels
+          * 10 - pennies
+          * 11 - dollars
+          * 12 - half dollars
+          */
+        public void ETextChanged(object sender, Android.Text.TextChangedEventArgs ex)
+        {
+            var changed = (EditText)sender;
+            var id = changed.Id;
+
+            EditText etHundreds = FindViewById<EditText>(Resource.Id.etHundreds);
+            TextView txtHundreds = FindViewById<TextView>(Resource.Id.txtHundredsTotal);
+
+            EditText etFifties = FindViewById<EditText>(Resource.Id.etFifties);
+            TextView txtFifties = FindViewById<TextView>(Resource.Id.txtFiftiesTotal);
+
+            EditText etTwenties = FindViewById<EditText>(Resource.Id.etTwenties);
+            TextView txtTwenties = FindViewById<TextView>(Resource.Id.txtTwentiesTotal);
+
+            EditText etTens = FindViewById<EditText>(Resource.Id.etTens);
+            TextView txtTens = FindViewById<TextView>(Resource.Id.txtTensTotal);
+
+            EditText etFives = FindViewById<EditText>(Resource.Id.etFives);
+            TextView txtFives = FindViewById<TextView>(Resource.Id.txtFivesTotal);
+
+            EditText etOnes = FindViewById<EditText>(Resource.Id.etOnes);
+            TextView txtOnes = FindViewById<TextView>(Resource.Id.txtOnesTotal);
+
+            EditText etTwos = FindViewById<EditText>(Resource.Id.etTwos);
+            TextView txtTwos = FindViewById<TextView>(Resource.Id.txtTwosTotal);
+
+            EditText etQuarters = FindViewById<EditText>(Resource.Id.etQuarters);
+            TextView txtQuarters = FindViewById<TextView>(Resource.Id.txtQuartersTotal);
+
+            EditText etDimes = FindViewById<EditText>(Resource.Id.etDimes);
+            TextView txtDimes = FindViewById<TextView>(Resource.Id.txtDimesTotal);
+
+            EditText etNickels = FindViewById<EditText>(Resource.Id.etNickels);
+            TextView txtNickels = FindViewById<TextView>(Resource.Id.txtNickelsTotal);
+
+            EditText etPennies = FindViewById<EditText>(Resource.Id.etPennies);
+            TextView txtPennies = FindViewById<TextView>(Resource.Id.txtPenniesTotal);
+
+            EditText etDollars = FindViewById<EditText>(Resource.Id.etDollars);
+            TextView txtDollars = FindViewById<TextView>(Resource.Id.txtDollarsTotal);
+
+            EditText etHalfDollars = FindViewById<EditText>(Resource.Id.etHalfDollars);
+            TextView txtHalfDollars = FindViewById<TextView>(Resource.Id.txtHalfDollarsTotal);
+
+            TextView txtTotal = FindViewById<TextView>(Resource.Id.txtTotalDisp);
+
+            ArrayList vals = Support.getValues(Support.parseValues(etHundreds, etFifties, etTwenties, etTens, etFives, etOnes, etTwos, 
+                                                                   etQuarters, etDimes, etNickels, etPennies, etDollars, etHalfDollars));
+            
+            if( id == Resource.Id.etHundreds)
             {
-                ArrayList vals = Support.getValues(Support.parseValues(etHundreds, etFifties, etTwenties, etTens, etFives, etOnes, etTwos,
-                                                        etQuarters, etDimes, etNickels, etPennies, etDollars, etHalfDollars));
+                txtHundreds.Text = vals[0].ToString();
+                txtTotal.Text = Support.calculateTotalRegister(vals).ToString();
+            }
+            else if( id == Resource.Id.etFifties)
+            {
+                txtFifties.Text = vals[1].ToString();
+                txtTotal.Text = Support.calculateTotalRegister(vals).ToString();
+            }
+            else if (id == Resource.Id.etTwenties)
+            {
+                txtTwenties.Text = vals[2].ToString();
+                txtTotal.Text = Support.calculateTotalRegister(vals).ToString();
+            }
+            else if (id == Resource.Id.etTens)
+            {
+                txtTens.Text = vals[3].ToString();
+                txtTotal.Text = Support.calculateTotalRegister(vals).ToString();
+            }
+            else if (id == Resource.Id.etFives)
+            {
+                txtFives.Text = vals[4].ToString();
+                txtTotal.Text = Support.calculateTotalRegister(vals).ToString();
+            }
+            else if (id == Resource.Id.etOnes)
+            {
+                txtOnes.Text = vals[5].ToString();
+                txtTotal.Text = Support.calculateTotalRegister(vals).ToString();
+            }
+            else if (id == Resource.Id.etTwos)
+            {
+                txtTwos.Text = vals[6].ToString();
+                txtTotal.Text = Support.calculateTotalRegister(vals).ToString();
+            }
+            else if (id == Resource.Id.etQuarters)
+            {
                 txtQuarters.Text = vals[7].ToString();
                 txtTotal.Text = Support.calculateTotalRegister(vals).ToString();
-            };
-
-            etDimes.TextChanged += (object sender, Android.Text.TextChangedEventArgs e) =>
+            }
+            else if (id == Resource.Id.etDimes)
             {
-                ArrayList vals = Support.getValues(Support.parseValues(etHundreds, etFifties, etTwenties, etTens, etFives, etOnes, etTwos,
-                                                        etQuarters, etDimes, etNickels, etPennies, etDollars, etHalfDollars));
                 txtDimes.Text = vals[8].ToString();
                 txtTotal.Text = Support.calculateTotalRegister(vals).ToString();
-            };
-
-            etNickels.TextChanged += (object sender, Android.Text.TextChangedEventArgs e) =>
+            }
+            else if (id == Resource.Id.etNickels)
             {
-                ArrayList vals = Support.getValues(Support.parseValues(etHundreds, etFifties, etTwenties, etTens, etFives, etOnes, etTwos,
-                                                        etQuarters, etDimes, etNickels, etPennies, etDollars, etHalfDollars));
                 txtNickels.Text = vals[9].ToString();
                 txtTotal.Text = Support.calculateTotalRegister(vals).ToString();
-            };
-
-            etPennies.TextChanged += (object sender, Android.Text.TextChangedEventArgs e) =>
+            }
+            else if (id == Resource.Id.etPennies)
             {
-                ArrayList vals = Support.getValues(Support.parseValues(etHundreds, etFifties, etTwenties, etTens, etFives, etOnes, etTwos,
-                                                        etQuarters, etDimes, etNickels, etPennies, etDollars, etHalfDollars));
                 txtPennies.Text = vals[10].ToString();
                 txtTotal.Text = Support.calculateTotalRegister(vals).ToString();
-            };
-
-            etDollars.TextChanged += (object sender, Android.Text.TextChangedEventArgs e) =>
+            }
+            else if (id == Resource.Id.etDollars)
             {
-                ArrayList vals = Support.getValues(Support.parseValues(etHundreds, etFifties, etTwenties, etTens, etFives, etOnes, etTwos,
-                                                        etQuarters, etDimes, etNickels, etPennies, etDollars, etHalfDollars));
                 txtDollars.Text = vals[11].ToString();
                 txtTotal.Text = Support.calculateTotalRegister(vals).ToString();
-            };
-
-            //etHalfDollars.TextChanged += ETextChanged;
-            
-            etHalfDollars.TextChanged += (object sender, Android.Text.TextChangedEventArgs e) =>
+            }
+            else if (id == Resource.Id.etHalfDollars)
             {
-                ArrayList vals = Support.getValues(Support.parseValues(etHundreds, etFifties, etTwenties, etTens, etFives, etOnes, etTwos,
-                                                        etQuarters, etDimes, etNickels, etPennies, etDollars, etHalfDollars));
                 txtHalfDollars.Text = vals[12].ToString();
                 txtTotal.Text = Support.calculateTotalRegister(vals).ToString();
-            };
-        }
+            }
 
-        /*public void ETextChanged( object sender, Android.Text.TextChangedEventArgs ex)
-        {
-            ArrayList vals = getValues(parseValues(etHundreds, etFifties, etTwenties, etTens, etFives, etOnes, etTwos,
-                                                        etQuarters, etDimes, etNickels, etPennies, etDollars, etHalfDollars));
-            txtHalfDollars.Text = vals[12].ToString();
-            txtTotal.Text = calculateTotalRegister(vals).ToString();
-        }*/
+        }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
